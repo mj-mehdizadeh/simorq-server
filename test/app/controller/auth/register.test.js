52876f5618724fa077957870514603a01a2c1a64
Mohammad Javad Mehdizadeh <mj.mehdizadeh93@gmail.com>
2018-12-27 17:23:50 +0330

    register tester and fix login tester

--- test/app/controller/auth/register.test.js
+++ test/app/controller/auth/register.test.js
@@ -2,9 +2,9 @@
 
 const { app } = require('egg-mock/bootstrap');
 
-describe('test/app/controller/auth/login.test.js', () => {
+describe('test/app/controller/auth/register.test.js', () => {
 
-  it('test phone code POST /auth/login', () => {
+  it('test phone code POST /auth/register', () => {
     return request()
       .expect(400)
       .expect({
@@ -13,8 +13,9 @@ describe('test/app/controller/auth/login.test.js', () => {
       });
   });
 
-  it('test phone number POST /auth/login', () => {
+  it('test phone number POST /auth/register', () => {
     app.mockService('sendCode', 'validateCode', () => true);
+    app.mockService('account', 'canRegister', () => false);
     return request()
       .expect({
         name: 'invalid_phone_number',
@@ -23,26 +24,31 @@ describe('test/app/controller/auth/login.test.js', () => {
       .expect(400);
   });
 
-  it('should POST /auth/login', () => {
+  it('should POST /auth/register', () => {
     app.mockService('sendCode', 'validateCode', () => true);
-    app.mockService('account', 'findByPhoneNumber', () => ({
+    app.mockService('account', 'canRegister', () => true);
+    app.mockService('room', 'insertRoom', () => ({ _id: '1' }));
+    app.mockService('account', 'insertAccount', () => ({
+      _id: '1',
       updateLoginHash: () => true,
       signedLoginToken: () => 'testHash',
     }));
-    return request().expect({
-      login_hash: 'testHash',
-    })
+    return request()
+      .expect({
+        login_hash: 'testHash',
+      })
       .expect(200);
   });
 });
 
 function request() {
   return app.httpRequest()
-    .post('/auth/login')
+    .post('/auth/register')
     .type('json')
     .send({
       phone_number: 98910,
       phone_hash: 'hash',
       phone_code: 11111,
+      title: 'tester',
     });
 }
