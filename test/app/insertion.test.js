9cca8c6f28838b6e7e3f49a144a41fef8cdab182
Mohammad javad Mehdizadeh <mj.mehdizadeh@kianiranian.com>
2019-02-18 14:46:03 +0330

    INSERTION BENCHMARK TEST

--- test/app/insertion.test.js
+++ test/app/insertion.test.js
@@ -2,34 +2,72 @@
 
 const { app } = require('egg-mock/bootstrap');
 const random = require('lodash').random;
+const map = require('lodash').map;
 
 describe('insertTest()', () => {
   // using generator function because of asynchronous invoking
   it('should test cool', async () => {
-    const accountId = '5c3dc5552b8d2a34acc9b614';
-    const ctx = app.mockContext();
-    for (let i = 1; i < 100; i++) {
-      const room = await ctx.service.room.insertRoom(`Room Test - ${i}`, 'just for test', 'GROUP', accountId);
-      await ctx.service.subscription.insertSubscribe(room.id, this.accountId, 'OWNER', room.type);
-      console.log('insertRoom', i);
-    }
+    // const accountId = '5c3dc5552b8d2a34acc9b615';
+    // const ctx = app.mockContext();
 
-    const subscribes = await ctx.service.subscription.getSubscribes(accountId);
-    const all = [];
-    subscribes.forEach(async subscribe => {
-      for (let i = 200; i < 400; i++) {
-        all.push(ctx.model.Message.create({
-          chatId: subscribe.chatId,
-          randomId: random(10000000000) + i,
-          text: `Message for Test - ${i}`,
-          createdBy: accountId,
-        }));
-        console.log('subscribes', subscribe.roomId, i);
-      }
-    });
-    await Promise.all(all);
-    console.log('done');
-    // const messages = await ctx.service.message.findLastIds(map(subscribes, 'chatId'));
-    // console.log('messages', messages);
+    // ## rooms queries
+    // const rooms = await ctx.model.Room.find({ createdBy: accountId });
+
+    // ## subscribes queries
+    // await ctx.model.Subscription.deleteMany({ createdBy: accountId });
+    // const subscribes = await ctx.service.subscription.findUserSubscribes(accountId);
+
+    // ## INSERT ROOMS
+    //
+    // const roomList = [];
+    // for (let i = 1; i < 10000; i++) {
+    //   roomList.push({
+    //     title: `benchmark - ${i}`, info: 'for benchmark',
+    //     type: 'GROUP',
+    //     createdBy: accountId,
+    //   });
+    // }
+    // console.time('insert rooms');
+    // const rooms = await ctx.model.Room.insertMany(roomList);
+    // console.timeEnd('insert rooms');
+
+    // ## INSERT SUBSCRIBES
+    //
+    // const subscribes = map(rooms, room => ({
+    //   roomId: room.id,
+    //   chatId: room.id,
+    //   createdBy: room.createdBy,
+    //   role: 'OWNER',
+    //   roomType: 'GROUP',
+    // }));
+    // console.time('insert subscribe');
+    // await ctx.model.Subscription.insertMany(subscribes);
+    // console.timeEnd('insert subscribe');
+
+    // ## INSERT MESSAGES
+    //
+    // const messagesList = map(subscribes, subscribe => {
+    //   const list = [];
+    //   for (let i = 1; i <= 400; i++) {
+    //     list.push({
+    //       chatId: subscribe.chatId,
+    //       randomId: random(11000000000) + i,
+    //       text: `Message for benchmark - ${i}`,
+    //       createdBy: accountId,
+    //     });
+    //   }
+    //   return list;
+    // });
+    //
+    // await insertMessages(0);
+    // async function insertMessages(i) {
+    //   if (i < messagesList.length) {
+    //     console.time('insert messages - ' + i);
+    //     await ctx.model.Message.insertMany(messagesList[i]);
+    //     console.timeEnd('insert messages - ' + i);
+    //     return insertMessages(++i);
+    //   }
+    // }
+
   });
 });
