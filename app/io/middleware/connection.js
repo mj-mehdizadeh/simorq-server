2805deb1b9fed7f9f3c64cdf3eb5ee3ceb6217c0
Mohammad Javad Mehdizadeh <mj.mehdizadeh93@gmail.com>
2019-03-30 13:18:34 +0430

    Hot fix socket connection

--- app/io/middleware/connection.js
+++ app/io/middleware/connection.js
@@ -4,9 +4,9 @@ module.exports = () => {
   return async (ctx, next) => {
     const token = ctx.socket.handshake.query.token;
     const model = await ctx.app.oAuth2Server.server.options.model.getAccessToken(token);
-    if (model.accountId) {
+    if (model && model.accountId) {
       ctx.io().join(ctx.socket, model.accountId);
-      const subscribes = await this.findUserSubscribes(model.accountId);
+      const subscribes = await ctx.service.subscription.findUserSubscribes(model.accountId);
       if (subscribes) {
         subscribes.forEach(subscribe => {
           ctx.io().join(ctx.socket, subscribe.chatId);
