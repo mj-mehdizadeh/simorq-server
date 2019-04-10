ecd19b5d27717d6dc6f74ec60ef03ffd80cb82a6
Mohammad javad Mehdizadeh <mj.mehdizadeh93@gmail.com>
2019-04-10 18:36:32 +0430

    Fix check contactsHash

--- app/controller/contacts/checkHash.js
+++ app/controller/contacts/checkHash.js
@@ -6,8 +6,10 @@ class CheckHashController extends Controller {
 
   async handle() {
     const contactsHash = this.getInput('contactsHash');
-    const userContactsHash = await this.ctx.service.account.findById(this.accountId, { contactsHash: 1 });
-    return contactsHash === userContactsHash ? 0 : 1;
+    const user = await this.ctx.service.account.findById(this.accountId, { contactsHash: 1 });
+    return {
+      equal: contactsHash === user.contactsHash,
+    };
   }
 }
 
