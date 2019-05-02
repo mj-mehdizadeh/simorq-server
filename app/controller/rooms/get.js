76ab340c918007558acf7117670c3824cb5d8c4e
Mohammad Javad Mehdizadeh <mj.mehdizadeh93@gmail.com>
2019-05-02 15:45:09 +0430

    get room by array of ids

--- app/controller/rooms/get.js
+++ app/controller/rooms/get.js
@@ -2,17 +2,20 @@
 
 const Controller = require('../../core/controller');
 const map = require('lodash').map;
+const uniq = require('lodash').uniq;
 
 class GetController extends Controller {
 
   async handle() {
-    const { roomId, chatId, skip, limit, username } = this.ctx.query;
+    const { roomIds, roomId, chatId, skip, limit, username } = this.ctx.query;
     if (roomId) {
       return this.findRoomById(roomId);
     } else if (chatId) {
       return this.findRoomByChatId(chatId);
     } else if (username && username.length >= 3) {
       return this.findRoomByUsername(username, skip, limit);
+    } else if (roomIds) {
+      return this.findInRoomIds(roomIds);
     }
     this.throwInvalidError('invalid_params');
   }
@@ -45,6 +48,20 @@ class GetController extends Controller {
     const rooms = await this.ctx.service.room.findUsername(username, skip, limit);
     const subscribes = await this.ctx.service.subscription.getRoomsSubscription(map(rooms, 'id'), this.accountId);
     return this.ctx.service.subscription.getSubscribesRoom(subscribes, this.accountId, rooms);
+  }
+
+  async findInRoomIds(roomIds) {
+    try {
+      const ids = JSON.parse(roomIds);
+      ids.forEach(id => {
+        if (!this.ctx.helper.matchId(id)) {
+          this.throwInvalidError('invalid_room_id');
+        }
+      });
+      return this.ctx.service.room.findInIds(uniq(ids));
+    } catch (e) {
+      this.throwInvalidError('invalid_room_ids');
+    }
   }
 }
 
